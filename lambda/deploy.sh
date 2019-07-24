8f10149f0051edd01cfca89e1dce65a715038e91
Van Boughner <van@viv.ai>
2019-07-24 15:42:54 -0700

    simplified lambda by removing src sub-folder

--- lambda/deploy.sh
+++ lambda/deploy.sh
@@ -1,7 +1,5 @@
 echo 'zipping up code...'
-rm index.zip
-cd src
-zip -q -r ../index.zip *
-cd .. 
+rm -f index.zip
+zip -q -r index.zip *
 echo 'updating lambda...'
 aws lambda update-function-code --function-name brain-lambda --zip-file fileb://index.zip
